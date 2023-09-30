a02fa1f3c302148ccdf57ad929c9b3e656b1ae13
rickmagz <ramnashor2@gmail.com>
2023-09-30 13:21:16 +0800

    Renamed files to PHP

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,15 @@
+<?php
+    include './db.php';
+    session_start();
+
+?>
 <!DOCTYPE html>
 <html data-bs-theme="light" lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Recommender System</title>
+    <title>Admin - Miagao One Negosyo Center</title>
     <link rel="icon" type="image/png" sizes="310x310" href="../assets/img/Miagao-logo.png">
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
@@ -20,12 +25,21 @@
                 <div class="card mb-5" style="background: var(--bs-card-cap-bg);border-style: none;">
                     <div class="card-body d-flex flex-column align-items-center" style="background: var(--bs-card-bg);border-radius: 24px;padding-bottom: 0px;padding-top: 36px;">
                         <div><img src="../assets/img/Miagao-logo.png" style="width: 100px;height: 100px;"><img src="../assets/img/DTI-LOGO.png" style="width: 100px;height: 100px;"><span style="font-size: 24px;color: var(--bs-blue);"><br><strong>Miagao One Negosyo Center</strong><br><strong>Administrators' Dashboard</strong></span></div>
-                        <form class="text-center" method="post" style="margin: 5px;">
+                        
+                        <form class="text-center" method="post" action="../admin/index.php" style="margin: 5px;">
                             <div class="row" style="padding-top: 28px;padding-bottom: 28px;">
-                                <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8 offset-sm-2 offset-md-2 offset-lg-2 offset-xl-2" style="padding-bottom: 12px;"><input class="form-control" type="email" name="email" placeholder="Email" autofocus="" required=""></div>
-                                <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8 offset-sm-2 offset-md-2 offset-lg-2 offset-xl-2"><input class="form-control" type="password" name="password" placeholder="Password"></div>
-                                <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6 offset-sm-3 offset-md-3 offset-lg-3 offset-xl-3 align-self-center" style="padding-top: 15px;"><button class="btn btn-primary d-block w-100" type="submit">Login</button></div>
+                                <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8 offset-sm-2 offset-md-2 offset-lg-2 offset-xl-2" style="padding-bottom: 12px;">
+                                    <input class="form-control" type="email" name="email" placeholder="Email" autofocus="" required="">
+                                </div>
+                                <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8 offset-sm-2 offset-md-2 offset-lg-2 offset-xl-2">
+                                    <input class="form-control" type="password" name="password" placeholder="Password">
+                                </div>
+                                <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6 offset-sm-3 offset-md-3 offset-lg-3 offset-xl-3 align-self-center" style="padding-top: 15px;">
+                                    <button class="btn btn-primary d-block w-100" type="submit">Login</button>
+                                </div>
                             </div>
+
+
                             <p class="text-muted"><br><span style="color: rgb(0, 0, 0);">©&nbsp;Miagao One Negosyo Center 2023. All Rights Reserved.</span><br><br></p>
                         </form>
                     </div>
