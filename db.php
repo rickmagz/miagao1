701ee2741a1c91993a3bb5448cbe3a7a24d07320
rickmagz <ramnashor2@gmail.com>
2023-09-30 11:43:17 +0800

    Added MYSQL connection

---
+++ db.php
@@ -0,0 +1,10 @@
+
+<?php
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_name = 'negosyo_center';
+$db_pass = '';
+
+$cxn = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("Can't connect to server.");
+?>
