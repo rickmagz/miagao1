c52d94637807c4a42a5c06a4d47ab5996f95585f
rickmagz <ramnashor2@gmail.com>
2023-10-05 16:02:08 +0800

    Change db location

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 
 $db_host = 'localhost';
 $db_user = 'root';
-$db_name = 'negosyo_center';
+$db_name = 'miagao_one';
 $db_pass = '';
 
 $cxn = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("Can't connect to server.");
