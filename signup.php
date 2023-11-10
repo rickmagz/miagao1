ecd9218cbd8229a42714a6009eb3c1c90306711a
rickmagz <ramnashor2@gmail.com>
2023-11-10 13:19:47 +0800

    Remove login/signup with Google

--- signup.php
+++ signup.php
@@ -59,11 +59,9 @@ include 'db.php';
                             <div class="row">
                                 <div class="col-xl-5">
                                     <button class="btn btn-primary border rounded-pill shadow" type="submit" style="margin-bottom: 10px; margin-right: 20px;" name="signup" form="signup">Create account</button>
-                                    <span>or</span>
+
                                 </div>
-                                <div class="col-xl-8"><button class="btn btn-primary border rounded-pill shadow d-xl-flex align-items-center align-content-center justify-content-xl-center align-items-xl-center" type="submit" style="color: var(--bs-btn-hover-border-color);background: var(--bs-btn-disabled-color);border-color: var(--bs-btn-active-border-color);"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" class="justify-content-xl-center align-items-xl-center" style="font-size: 20px;">
-                                            <path d="M6 12C6 15.3137 8.68629 18 12 18C14.6124 18 16.8349 16.3304 17.6586 14H12V10H21.8047V14H21.8C20.8734 18.5645 16.8379 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C15.445 2 18.4831 3.742 20.2815 6.39318L17.0039 8.68815C15.9296 7.06812 14.0895 6 12 6C8.68629 6 6 8.68629 6 12Z" fill="currentColor"></path>
-                                        </svg>&nbsp;Sign up with Google</button></div>
+
                             </div>
                         </div>
                         <p class="text-muted">Have an account? <a href="login.php">Log in&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-arrow-narrow-right">
