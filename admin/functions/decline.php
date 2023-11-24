433c2c97640404f31dcadb36fc7a42fbb2ecb515
rickmagz <ramnashor2@gmail.com>
2023-11-24 10:06:58 +0800

    Change status from pending to decline

--- admin/functions/decline.php
+++ admin/functions/decline.php
@@ -5,6 +5,6 @@ include '../db.php';
 
 $id = $_GET['id'];
 
-$decline = mysqli_query($cxn, "UPDATE entrep SET status='PENDING' WHERE entrep_id='$id'") or die("Error in query: $accept" . mysqli_error($cxn));
+$decline = mysqli_query($cxn, "UPDATE entrep SET status='DECLINED' WHERE entrep_id='$id'") or die("Error in query: $accept" . mysqli_error($cxn));
 
 echo "<script type='text/javascript'>alert('Request Decline.'); location.href='../dashboard.php';</script>";
