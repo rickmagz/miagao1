39fd1fb22cfaed07bcd0ed871e7e5e24dba23339
rickmagz <ramnashor2@gmail.com>
2023-11-28 03:08:08 +0800

    File location fixed

--- user/products.php
+++ user/products.php
@@ -165,7 +165,7 @@ if ($get_product_info->num_rows > 0) {
                 ?>
                             <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
                                 <div class="card">
-                                    <img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="../assets/img/<?php echo $pr['product_image']; ?>" />
+                                    <img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="../assets/img/product_img/<?php echo $pr['product_image']; ?>" />
                                     <div class="card-body p-2 text-center">
                                         <p class="text-primary card-text mb-0"><?php echo $pr['product_type']; ?></p>
                                         <h5 class="card-title"><?php echo $pr['product_name']; ?></h5>
