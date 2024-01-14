6f2da547cf246579f5230ac10c5c6703d43ded73
rickmagz <ramnashor2@gmail.com>
2024-01-14 16:01:04 +0800

    Added initial search function

---
+++ user/search.php
@@ -0,0 +1,163 @@
+<?php
+session_start();
+include '../db.php';
+
+$user_id = $_SESSION['user_id'];
+
+?>
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Miagao Negosyo Center</title>
+    <link rel="icon" type="image/png" sizes="310x310" href="../assets/img/Miagao-logo.png">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="../assets/css/Hero-Clean-images.css">
+    <link rel="stylesheet" href="../assets/css/Navbar-Right-Links-icons.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg sticky-top shadow navbar-shrink navbar-light" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="browse.php">
+                <img src="../assets/img/miagao_one.png" width="85" height="70" /><span style="margin-left: 5px;">One Miagao<br />Negosyo</span>
+            </a>
+            <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active" href="./browse.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./business.php">Business</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./products.php">Products</a></li>
+                    <li class="nav-item fw-bold d-flex align-items-center">
+                        <div class="nav-item dropdown bg-primary border rounded-pill border-primary shadow d-flex align-items-center" style="padding-left: 14px;padding-right: 14px;"><a class="dropdown-toggle link-light" aria-expanded="false" data-bs-toggle="dropdown" href="#">Menu</a>
+                            <div class="dropdown-menu text-break border rounded" style="margin-bottom: 0px;padding-left: 0px;padding-right: 0px;margin-right: 0px;margin-top: 0px;padding-top: 8px;padding-bottom: 8px;"><a class="dropdown-item" href="#" style="display: flex;overflow: hidden;"><img width="30" height="30" src="../assets/img/Screenshot_2021-01-28-04-41-56-92.jpg">&nbsp;<strong><?php echo $_SESSION['firstname']; ?></strong></a><a class="dropdown-item" href="./accountsettings.php">Account Settings</a><a class="dropdown-item" href="../logout.php">Log out</a></div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+    <!-- search_bar -->
+    <section class="mt-4">
+        <div class="container" style="max-width: 50%;">
+            <h4 class="card-title text-center">Looking for something?</h4><br />
+            <form action="search.php" method="POST">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search here..." name="search" required>
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" type="submit" name="search">Search</button>
+                    </span>
+            </form>
+        </div>
+    </section>
+
+    <!-- search results -->
+    <section data-aos="fade-up">
+        <div class="container py-4 py-xl-5">
+            <div class="row mb-5">
+                <div class="col-md-8 col-xl-6 text-center mx-auto">
+                    <h2><strong>Search results</strong></h2>
+                </div>
+                <h2><strong>Products</strong></h2>
+            </div>
+            <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3" style="margin-top: -65px;">
+
+                <?php
+                if (isset($_POST['search'])) {
+                    $search = $_POST['search'];
+
+                    $prod_search_query = mysqli_query($cxn, "SELECT * FROM product_list WHERE product_name LIKE '%$search%' ORDER BY product_name asc") or die("Error in query: $prod_search_query." . mysqli_error($cxn));
+
+                    if ($prod_search_query->num_rows > 0) {
+                        while ($p = $prod_search_query->fetch_assoc()) {
+
+                ?>
+                            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                                <div class="card">
+                                    <img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="../assets/img/business_img/<?php echo $p['product_image']; ?>" />
+                                    <div class="card-body p-2 text-center">
+                                        <h5 class="card-title"><?php echo $p['product_name']; ?></h5>
+                                    </div>
+                                    <div class="card-footer text-body-secondary text-center">
+                                        <a class="btn btn-outline-primary btn-sm border-primary rounded-pill mt-1" href="./product_view.php?id=<?php echo $p['product_id']; ?>" target="_self">More Info</a>
+                                    </div>
+                                </div>
+                            </div>
+                <?php
+                        }
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </section>
+
+    <section data-aos="fade-up">
+        <div class="container py-4 py-xl-5">
+            <div class="row mb-5">
+                <h2><strong>Business</strong></h2>
+            </div>
+            <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3" style="margin-top: -65px;">
+
+                <?php
+                if (isset($_POST['search'])) {
+                    $search = $_POST['search'];
+
+                    $busi_search_query = mysqli_query($cxn, "SELECT * FROM business_list WHERE business_name LIKE '%$search%' ORDER BY business_name asc") or die("Error in query: $busi_search_query." . mysqli_error($cxn));
+
+                    if ($busi_search_query->num_rows > 0) {
+                        while ($b = $busi_search_query->fetch_assoc()) {
+
+
+                ?>
+                            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                                <div class="card">
+                                    <img class="card-img-top w-100 d-block fit-cover" style="height: 200px;" src="../assets/img/business_img/<?php echo $b['business_image']; ?>" />
+                                    <div class="card-body p-2 text-center">
+                                        <h5 class="card-title"><?php echo $b['business_name']; ?></h5>
+                                    </div>
+                                    <div class="card-footer text-body-secondary text-center">
+                                        <a class="btn btn-outline-primary btn-sm border-primary rounded-pill mt-1" href="./business_view.php?id=<?php echo $b['business_id']; ?>" target="_self">More Info</a>
+                                    </div>
+                                </div>
+                            </div>
+                <?php
+                        }
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </section>
+
+
+    <footer>
+        <div class="container py-4 py-lg-5">
+            <hr>
+            <div class="text-muted d-flex justify-content-between align-items-center pt-3">
+                <p class="mb-0">Copyright © 2023 One Negosyo Miagao<br /></p>
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item"><img src="../assets/img/miagao_one.png" width="85" height="70" /></li>
+
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="../assets/js/bs-init.js"></script>
+    <script src="../assets/js/main.js"></script>
+
+</body>
+
+</html>
